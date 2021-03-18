3cd96f0c6dfeba0574ce304d4a94937caad694b7
AbdouMchayaa <abderrahmen.mchayaa@gmail.com>
2021-03-18 15:53:23 +0100

    Remap User ORM

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,6 +9,9 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use JMS\Serializer\Annotation as Serializer;
 
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
 class User implements userInterface
 {
     /**
