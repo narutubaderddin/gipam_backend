83bde674b4a70ac879b73a6cbef4b952c85b45d6
mabouchriha <mabouchriha@dom.tti>
2021-04-16 11:28:12 +0100

    denomination api

--- src/Repository/StyleRepository.php
+++ src/Repository/StyleRepository.php
@@ -14,6 +14,7 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class StyleRepository extends ServiceEntityRepository
 {
+    use RepositoryTrait;
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Style::class);
