888c67fbef473a5b60140c2d0924ffcd65cf2cc1
AbdouMchayaa <abderrahmen.mchaaya@talan.com>
2021-04-20 15:43:25 +0100

    BuildingController ok

--- src/Repository/BuildingRepository.php
+++ src/Repository/BuildingRepository.php
@@ -14,6 +14,8 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class BuildingRepository extends ServiceEntityRepository
 {
+    use RepositoryTrait;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Building::class);
