79cd674ff69420b5540fc5809a4040a0912c96e2
AbdouMchayaa <abderrahmen.mchaaya@talan.com>
2021-05-11 17:24:38 +0100

    add people field to author creation

--- src/Controller/API/PersonController.php
+++ src/Controller/API/PersonController.php
@@ -7,6 +7,7 @@ use App\Exception\FormValidationException;
 use App\Form\PersonType;
 use App\Model\ApiResponse;
 use App\Services\ApiManager;
+use FOS\RestBundle\Context\Context;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\Annotations\Route;
@@ -146,19 +147,25 @@ class PersonController extends AbstractFOSRestController
      * @Rest\QueryParam(name="website",map=true, nullable=false, description="filter by website. example: website[eq]=value")
      * @Rest\QueryParam(name="phone",map=true, nullable=false, description="filter by phone. example: phone[eq]=value")
      * @Rest\QueryParam(name="email",map=true, nullable=false, description="filter by email. example: email[eq]=value")
+     * @Rest\QueryParam(name="active",map=true, nullable=false, description="filter by active. example: active[eq]=value")
      * @Rest\QueryParam(name="author", nullable=false, description="filter by author id. example: author[eq]=value")
      * @Rest\QueryParam(name="search", map=false, nullable=true, description="search. example: search=text")
      *
-     * @Rest\View(serializerGroups={"id", "person", "short", "response"})
+     * @Rest\View()
      *
      * @param ParamFetcherInterface $paramFetcher
-     *
+     * @param Request $request
      * @return View
      */
-    public function listPersons(ParamFetcherInterface $paramFetcher)
+    public function listPersons(ParamFetcherInterface $paramFetcher, Request $request)
     {
+        $serializerGroups = $request->get('serializer_group', '["id", "person", "short"]');
+        $serializerGroups = json_decode($serializerGroups, true);
+        $serializerGroups[] = "response";
+        $context = new Context();
+        $context->setGroups($serializerGroups);
         $records = $this->apiManager->findRecordsByEntityName(Person::class, $paramFetcher);
-        return $this->view($records, Response::HTTP_OK);
+        return $this->view($records, Response::HTTP_OK)->setContext($context);
     }
 
     /**
--- src/Form/AuthorType.php
+++ src/Form/AuthorType.php
@@ -4,9 +4,11 @@ namespace App\Form;
 
 use App\Entity\Author;
 use App\Entity\AuthorType as Type;
+use App\Entity\Person;
 use App\Form\Type\BooleanType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -23,7 +25,18 @@ class AuthorType extends AbstractType
                     'class' => Type::class,
                     'choice_label' => 'id'
                 ]
-            );
+            )
+            ->add('people', CollectionType::class, array(
+                'entry_type' => EntityType::class,
+                'entry_options' => array('class' => Person::class,
+                    'label' => false,
+                    'choice_label' => 'id',
+                ),
+                'allow_delete' => true,
+                'allow_add' => true,
+                'prototype' => true,
+                'by_reference' => false,
+            ));
     }
 
     public function configureOptions(OptionsResolver $resolver)
