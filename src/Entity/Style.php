c9090f3e6143105bde9f9857207d084a8282af75
AbdouMchayaa <abderrahmen.mchaaya@talan.com>
2021-05-19 05:22:05 +0100

    add unicity to style

--- src/Entity/Style.php
+++ src/Entity/Style.php
@@ -8,10 +8,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as JMS;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=StyleRepository::class)
  * @ORM\Table(name="style")
+ * @UniqueEntity("label", repositoryMethod="iFindBy", message="Un style avec ce libellé existe déjà!")
  */
 class Style
 {
