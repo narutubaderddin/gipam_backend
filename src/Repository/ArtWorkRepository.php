1fc79d4bf49b3ae94e8b0b4f6f6f772eb86bf1ec
FARES AMDOUNI <fares.amdouni@talan.com>
2021-05-21 14:36:28 +0000

    remove distinct

--- src/Repository/ArtWorkRepository.php
+++ src/Repository/ArtWorkRepository.php
@@ -174,7 +174,6 @@ class ArtWorkRepository extends ServiceEntityRepository
             //                        ->leftJoin('sub_divisions.services','services')
         ;
         $query->andWhere('artWork.isCreated = true');
-        $query->distinct('artWork.id');
         if ($countTotal) {
             return $query;
         }
